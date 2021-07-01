4a42fbbc4faa548434f287876d385a5c1217153a
Gabriel Santos <gabriel.st@outlook.com>
2021-07-02 00:10:56 +0100

    feat: Pass The Skills and Certification to separate components

--- src/components/Sections/Experience/ExperienceList.js
+++ src/components/Sections/Experience/ExperienceList.js
@@ -1,23 +1,19 @@
 import React from 'react';
 
 import Experience from './Experience';
+import Section from '../';
 
 const ExperienceList = props => {
   const { config } = props;
   return (
-    <div className="blur-bg">
-      <section
-        className="resume-section p-3 p-lg-5 d-flex justify-content-center"
-        id="experience"
-      >
-        <div className="w-100">
-          <h2 className="mb-5">Experience</h2>
-          {config.experiences.map(e => {
-            return <Experience experience={e} />;
-          })}
-        </div>
-      </section>
-    </div>
+    <Section name="experience">
+      <div className="w-100">
+        <h2 className="mb-5">Experience</h2>
+        {config.experiences.map(e => {
+          return <Experience experience={e} />;
+        })}
+      </div>
+    </Section>
   );
 };
 
